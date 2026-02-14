25081592c9d01178bf448ac8a4fbc5893ee51e8b
Assed Mustapha Kacemi <assed@Asseds-MacBook-Pro.local>
2026-02-14 14:55:59 +0100

    create ex1 folder

--- python01/ex0/ft_garden_intro.py
+++ python01/ex0/ft_garden_intro.py
@@ -1,11 +1,18 @@
 
-def main():
+def ft_garden_intro() -> None:
+    color: str = "Rose"
+    height: int = 25
+    age: int = 30
     print("=== Welcome to My Garden ===")
-    print("Plant: Rose")
-    print("Height: 25cm")
-    print("Age: 30 days")
+    print(f"Plant: {color}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
     print("")
     print("=== End of Program ===")
+
+
+def main() -> None:
+    ft_garden_intro()
 
 
 if __name__ == "__main__":
