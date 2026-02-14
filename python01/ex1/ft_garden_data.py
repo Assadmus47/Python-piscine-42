72e711f68d4a1ee54e66a0e690b0c1b06bd27f23
Assadmus47 <assadkac47@gmail.com>
2026-02-14 18:46:19 +0100

    finishing exi 0 1 2

---
+++ python01/ex1/ft_garden_data.py
@@ -0,0 +1,32 @@
+
+class Plant:
+    """
+    Represent a plant with name , height and age.
+    """
+    def __init__(self, name: str, height: int, age: int):
+        self.height = height
+        self.name = name
+        self.age = age
+
+    def get_info(self) -> None:
+        """
+        Display formatted info about the plants.
+        """
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+def main() -> None:
+    plants = [
+        Plant("Rose", 25, 30),
+        Plant("Sunflower", 80, 45),
+        Plant("Cactus", 15, 120)
+    ]
+
+    print("=== Garden Plant Registry ===")
+
+    for plant in plants:
+        plant.get_info()
+
+
+if __name__ == "__main__":
+    main()
