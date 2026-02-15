09905c27a86d02549b09deeedac79e9686756cc9
Assed Mustapha Kacemi <assed@Asseds-MacBook-Pro.local>
2026-02-15 02:26:33 +0100

    finishing exo1

---
+++ python01/ex2/ft_plant_growth.py
@@ -0,0 +1,46 @@
+
+class Plant:
+    """
+    Represent a plant with name , height and age.
+    """
+    def __init__(self, name: str, height: int, day: int):
+        self.height = height
+        self.name = name
+        self.day = day
+
+    def get_info(self) -> None:
+        """
+        Display formatted info about the plants
+        """
+        print(f"{self.name}: {self.height}cm, {self.day} days old")
+
+    def age(self) -> None:
+        """
+        increment age of plants by one day
+        """
+        self.day += 1
+
+    def grow(self) -> None:
+        """
+        increment height of plants by one cm
+        """
+        self.height += 1
+
+
+def main() -> None:
+    plants = [
+        Plant("Rose", 25, 30),
+        Plant("Sunflower", 80, 45),
+        Plant("Cactus", 15, 120)
+    ]
+
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        for plant in plants:
+            plant.age()
+            plant.grow()
+            plant.get_info()
+
+
+if __name__ == "__main__":
+    main()
