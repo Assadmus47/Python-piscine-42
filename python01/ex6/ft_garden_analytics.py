7cad5fb88c64a39138a4a5702f57686161467030
Assed Mustapha Kacemi <assed@Asseds-MacBook-Pro.local>
2026-02-16 15:56:20 +0100

    finishing exo5 and starting exo6

---
+++ python01/ex6/ft_garden_analytics.py
@@ -0,0 +1,61 @@
+
+class Plant:
+    """
+    Represent a plant with name and height.
+    """
+    def __init__(self, name: str, height: int):
+        self.height = height
+        self.name = name
+
+    def get_info(self) -> None:
+        """
+        Display formatted info about the plants
+        """
+        print(f"- {self.name}: {self.height}cm")
+
+    def grew(self) -> None:
+        """
+        Dsiplay that plant is grewing and increment height
+        """
+        print(f"{self.name} grew 1cm")
+        self.height += 1
+
+class FloweringPlant(Plant):
+    """
+    Represent a flower with name , height and color.
+    """
+    def __init__(self, name: str, height: int, color: str):
+        super().__init__(name, height)
+        self.color = color
+
+    def get_info(self) -> None:
+        """
+        Display detailed information about the flower
+        """
+        print(f"- {self.name}: {self.height}cm, {self.color} flowers (blooming)")
+
+
+class PrizeFlower(FloweringPlant):
+    """
+    Represent a prizeFlower with name , height , age , color and prize points.
+    """
+    def __init__(self, name: str, height: int, color: str,  prize_points: int):
+        super().__init__(name, height, color)
+        self.prize_points =  prize_points
+
+    def get_info(self) -> None:
+        """
+        Display detailed information about the flower
+        """
+        print(f"- {self.name}: {self.height}cm, {self.color} flowers (blooming), Prize points: {self.prize_points}")
+
+
+class GardenManager:
+    total_gardens = 0
+
+    class GardenStats:
+    
+    @classmethod
+    def create_garden_network(cls):
+        total_gardens += 1
+        return cls("new garden")
