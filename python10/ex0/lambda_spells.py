e9813a773ca46994935fa0104792b0d918c08df5
Assadmus47 <assadkac47@gmail.com>
2026-03-15 05:38:41 +0100

    making first steps to first exo python10

---
+++ python10/ex0/lambda_spells.py
@@ -0,0 +1,15 @@
+
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    pass
+
+liste = {
+    2: "salut",
+    3: "cava",
+    5: "tres bien",
+    1:"et toi"
+}
+
+print(liste)
+sorted(liste)
+print(liste)
+
